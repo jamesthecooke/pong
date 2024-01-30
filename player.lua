0bfd5ee7d1c63453cd9b22b48675cbde793c6397
jamesthecooke <jamesjcooke03@gmail.com>
2024-01-30 11:54:27 +0000

    added player paddel movements and boundries

--- player.lua
+++ player.lua
@@ -8,7 +8,26 @@ function Player:load()
     self.speed = 500
 end
 
+
 function Player:update(dt)
+
+    self:move(dt)
+    self:checkBoundaries()
+
+end
+
+
+function Player:draw()
+
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+
+end
+
+
+
+
+function Player: move(dt)
+    --  moving the player
     if love.keyboard.isDown("w") then
         self.y = self.y - self.speed * dt
     elseif love.keyboard.isDown("s") then
@@ -16,6 +35,11 @@ function Player:update(dt)
     end
 end
 
-function Player:draw()
-    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+function Player: checkBoundaries()
+    --  stopping the player from going off the screen
+    if self.y < 0 then
+        self.y = 0
+    elseif self.y + self.height > love.graphics.getHeight() then
+        self.y = love.graphics.getHeight() - self.height
+    end
 end
